dbaad60459441dc2ed17020626d4db2e2c8aff22
Kyle Heitman <kyle.heitman@uconn.edu>
2015-01-21 16:35:11 -0500

    Include files
    File includes that the botnet needs to run

---
+++ includes.h
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <iostream>
+#include <winsock.h>
+#include <string>
+
+#include <ctype.h> //Needed for toascii in rqKEYLOG.h
+#pragma comment(lib, "wsock32.lib")
+
+#include <iostream>
+#include <fstream>
+
+#include "defines.h"
+#include "configs.h"
+#include "rqIRC.h"
+#include "rqHTTP.h"
+#include "rqKEYLOG.h"
+#include "userInfo.h"
+#include "youtube.h"
