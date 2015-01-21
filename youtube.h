b7a3b05b93d6edb02cbaea87a952f4d0bac4d28a
Kyle Heitman <kyle.heitman@uconn.edu>
2015-01-21 16:38:32 -0500

    Useless atm

---
+++ youtube.h
@@ -0,0 +1,20 @@
+#ifdef useYoutube
+
+#ifndef YOUTUBE_HTTP
+#define YOUTUBE_HTTP
+
+
+class youtube {
+private:
+	std::string site;
+	WSADATA wsaData;
+public:
+	youtube(){};
+
+
+};
+
+
+
+#endif
+#endif
